6c52598a009675daa1314ec7b76d292fa5584219
Casper Huang <casper.w.huang@qq.com>
2023-05-04 21:03:15 +0800

    deploy

--- components/LinkItem.vue
+++ components/LinkItem.vue
@@ -26,41 +26,19 @@ const { isDark } = useData()
 </script>
 
 <template>
-  <a
-    :href="props.link"
-    target="_blank"
-    p-l-4
-    min-h-144px
-    w-auto
-    flex="~ col justify-start"
-    select-none
-    :filter="isDark ? 'hover:brightness-130' : 'hover:brightness-90'"
-  >
+  <a :href="props.link" target="_blank" p-l-4 min-h-144px w-auto flex="~ col justify-start"
+    select-none :filter="isDark ? 'hover:brightness-130' : 'hover:brightness-90'">
     <div flex="~ items-center">
-      <div
-        class="item-title-icon"
-        p-3
-      >
+      <div class="item-title-icon" p-3>
         <slot name="icon" />
       </div>
-      <div
-        class="item-title-text"
-        text="5 w-5"
-        h-7
-      >
+      <div class="item-title-text" text="5 w-5" h-7>
         {{ props.title }}
       </div>
     </div>
-    <div
-      p-1
-      overflow-hidden
-    >
-      <Badge
-        :type="'info'"
-        v-for="tag in props.tags"
-        :key="tag"
-        class="m-r-1 !bg-transparent"
-      >
+    <div p-1 overflow-hidden>
+      <Badge :type="'info'" v-for="tag in props.tags" :key="tag"
+        class="m-r-1 !bg-transparent">
         {{ tag }}
       </Badge>
     </div>
