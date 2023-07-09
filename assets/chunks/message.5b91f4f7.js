b794bf5a25cd6c38541b1c8f86700c2dfa701205
Casper Huang <casper.w.huang@qq.com>
2023-07-09 17:13:39 +0800

    basic framework complete

---
+++ assets/chunks/message.5b91f4f7.js
@@ -0,0 +1,1 @@
+import{r as s}from"./framework.b2927391.js";const e=s(),a=()=>e.value;export{e as m,a as u};
---
+++ assets/chunks/theme.8a23b2b0.js
@@ -0,0 +1,1 @@
+import{r}from"./framework.b2927391.js";const e=r("dark");export{e as t};
