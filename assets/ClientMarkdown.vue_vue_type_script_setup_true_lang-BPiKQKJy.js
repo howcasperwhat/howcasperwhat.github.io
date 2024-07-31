870d4e42c83be99fff3091513a6d6c24f9116cd7
howcasperwhat <casper.w.huang@qq.com>
2024-07-31 16:19:34 +0800

    Updates

--- assets/ClientMarkdown.vue_vue_type_script_setup_true_lang-BPiKQKJy.js
+++
@@ -1,1 +0,0 @@
-import{u as r}from"./markdown-DFKfk4Ej.js";import{d as s,A as c,e as a,c as m}from"./app-DYSJX45E.js";const p=["innerHTML"],u=s({__name:"ClientMarkdown",props:{content:{}},setup(n){const{md:e}=r(),o=n,t=c(()=>e.render(o.content));return(d,i)=>(a(),m("div",{class:"markdown client",innerHTML:t.value},null,8,p))}});export{u as _};
