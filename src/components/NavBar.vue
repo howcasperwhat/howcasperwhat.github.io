7f05cafd2195c3e838b5711aa798e246400e807f
Casper Huang <casper.w.huang@qq.com>
2025-03-23 22:33:24 +0800

    chore: simplify navbar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,26 +1,26 @@
 <script setup lang='ts'>
 import { useThemeStore } from '../stores/theme'
-import { useConfigStore } from '../stores/config'
+// import { useConfigStore } from '../stores/config'
 import Logo from './Logo.vue'
 
 const { toggleTheme } = useThemeStore()
-const { githubURL } = useConfigStore()
+// const { githubURL } = useConfigStore()
 </script>
 
 <template>
-  <header w-full box-border p-y-8 p-x-6 grid="~ flow-col items-center">
+  <header w-full box-border p-x-2 p-y-6 grid="~ flow-col items-center">
     <RouterLink lg:fixed absolute to="/">
       <Logo />
     </RouterLink>
     <nav flex="~ gap-4 items-center justify-end">
-      <RouterLink to="/notes/">
+      <!-- <RouterLink to="/notes/">
         <span lt-md:hidden v-text="'Notes'" />
         <div i-carbon:notebook text-5 md-hidden />
       </RouterLink>
       <RouterLink to="/demos/">
         <span lt-md:hidden v-text="'Demos'" />
         <div i-carbon:workspace text-5 md-hidden />
-      </RouterLink>
+      </RouterLink> -->
       <!-- <RouterLink to="/study/">
         <span lt-md:hidden v-text="'Study'" />
         <div i-carbon:growth text-5 md-hidden />
@@ -28,9 +28,6 @@ const { githubURL } = useConfigStore()
       <button @click="toggleTheme">
         <div i-carbon:sun dark:i-carbon-moon text-5 />
       </button>
-      <a :href="githubURL" target="_blank" class="icon">
-        <div i-iconoir:github text-5 />
-      </a>
     </nav>
   </header>
 </template>
