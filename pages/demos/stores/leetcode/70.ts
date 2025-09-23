631cb69bb68589c7121e4cbc7cf402ef85f64415
Casper Huang <casper.w.huang@qq.com>
2025-09-23 22:25:37 +0800

    chore(leetcode): add `Q70`

---
+++ pages/demos/stores/leetcode/70.ts
@@ -0,0 +1,8 @@
+export function climbStairs(n: number): number {
+  const dp = Array.from({ length: n }, () => 1)
+  n > 1 && (dp[1] = 2)
+  for (let i = 2; i < n; ++i) {
+    dp[i] = dp[i - 1] + dp[i - 2]
+  }
+  return dp.at(-1)!
+}
